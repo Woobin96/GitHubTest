060636c8799657313f5ebdabccd76d887361cf18
Woobin96 <78577067+Woobin96@users.noreply.github.com>
2021-02-05 14:20:13 +0900

    Update MainActivity.java

--- app/src/main/java/com/wooeun18/githubtest/MainActivity.java
+++ app/src/main/java/com/wooeun18/githubtest/MainActivity.java
@@ -12,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().setTitle("Git Hub");
+        
+        int a;
+        int b;
 
     }
 }
